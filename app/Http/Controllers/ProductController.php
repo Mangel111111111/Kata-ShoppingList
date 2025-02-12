ea2c9d10795dbdf2f05397103d5efbe77e88302a
Miguel Ángel García López <mangel111111111@gmail.com>
2025-02-12 10:21:59 +0100

    feat: implement CRUD operations in ProductController and update API routes

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Illuminate\Http\Request;
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
 
@@ -13,54 +14,71 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $product = Product::all();
+        return response()->json($product, 200);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    /* public function create()
     {
         //
-    }
+    } */
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreProductRequest $request)
+    public function store(Request $request)
     {
-        //
+        $product = Product::create([
+            'ProductName' => $request->ProductName,
+        ]);
+        $product->save();
+        return response()->json($product, 200);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Product $product)
+    /* public function show(Product $product)
     {
         //
-    }
+    } */
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Product $product)
+    /* public function edit(Product $product)
     {
         //
-    }
+    } */
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateProductRequest $request, Product $product)
+    public function update(Request $request, string $id)
     {
-        //
+        $product = Product::find($id);
+        
+        $product->update([
+            'ProductName' => $request->ProductName,
+        ]);
+        $product->save();
+        return response()->json($product, 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Product $product)
+    public function destroy(string $id)
     {
-        //
+        $product = Product::find($id);
+
+        $product->delete();
+
+        return response()
+        ->json(['message' => 'Product deleted successfully'], 200);
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/products',[ProductController::class,'index'])->name('apihome');
+Route::delete('/products/{id}',[ProductController::class,'destroy'])->name('apidestroy');
+Route::post('/products', [ProductController::class,'store'])->name('apistore');
+Route::put('/products/{id}',[ProductController::class, 'update'])->name('apiupdate');
